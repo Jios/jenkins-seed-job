05171057f9e3b8f8eb42a5a058c01e4c031c55ca
jian li <jian_li@tutk.com>
2016-09-13 17:08:16 +0800

    steps: move env settings to build step

--- utilities/Steps.groovy
+++ utilities/Steps.groovy
@@ -2,18 +2,29 @@ package utilities
 
 class Steps
 {
-	def properties_file = 'properties/postbuild.properties'
-	
 	void setBuildScript (def context, sh_command)
 	{
+		String properties_file = '${WORKSPACE}/properties/prebuild.properties'
+		def sh_script = '''mkdir -p ${WORKSPACE}/properties 
+						  |touch ${WORKSPACE}/properties/prebuild.properties
+						  |touch ${WORKSPACE}/properties/postbuild.properties 
+						  |'''.stripMargin()
     	context.steps 
         {
+        	shell(sh_script)
+        	envInjectBuilder 
+			{
+				propertiesFilePath(properties_file)
+				propertiesContent('')
+			}
             shell(sh_command)
         }
 	}
 
 	void setEnvInjectBuilder(def context)
 	{
+		String properties_file = '${WORKSPACE}/properties/postbuild.properties'
+
 		context.steps 
 		{
 			envInjectBuilder 
