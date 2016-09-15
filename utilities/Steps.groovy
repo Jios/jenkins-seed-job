504b487d45e426a320bba64e593de567e655b91b
jian <li822jh@gmail.com>
2016-09-16 00:26:51 +0800

    steps: update shell script

--- utilities/Steps.groovy
+++ utilities/Steps.groovy
@@ -5,7 +5,9 @@ class Steps
 	void setBuildScript (def context, sh_command)
 	{
 		String properties_file = '${WORKSPACE}/properties/prebuild.properties'
-		def sh_script = '''mkdir -p ${WORKSPACE}/properties 
+		
+		def sh_script = '''rm -rf ${WORKSPACE}/properties 
+						  |mkdir -p ${WORKSPACE}/properties 
 						  |touch ${WORKSPACE}/properties/prebuild.properties
 						  |touch ${WORKSPACE}/properties/postbuild.properties 
 						  |'''.stripMargin()
