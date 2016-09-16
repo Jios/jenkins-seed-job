f492ccd2227a97d98f37d0a524945bc8dfabbec1
jian <li822jh@gmail.com>
2016-09-16 18:53:44 +0800

    properties: add env variables

--- global-properties.groovy
+++ global-properties.groovy
@@ -41,6 +41,13 @@ def slack_test_token = "xoxp-15730259699-15737121729-17130785383-885938a11d"
 def slack_jenkins_token = "nLIgN4qKWOKH32e5mXiuKCww"
 def slack_fastlane_token = "https://hooks.slack.com/services/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"
 
+def git_http_host = "http://stash.tutk.com:7990"
+def git_ssh_host  = "ssh://git@stash.tutk.com:7999"
+
+// Organizations: TUTK RD1
+def crashlytics_api_key      = "a903f72a6ffdd06539302df50101d05b48d830ea"
+def crashlytics_build_secret = "039b2c50ea29ef03f71f5cb93b598bfdfe4a19a23054f66cdddd13275803e599"
+
 // env variable map
 def envMap = [:]
 envMap = ["SLACK_PYTHON_TOKEN": slack_test_token,
@@ -49,8 +56,13 @@ envMap = ["SLACK_PYTHON_TOKEN": slack_test_token,
 		  "SLACK_TEAM": "tutk-kalay",
 		  "SLACK_CHANNEL": "#build",
 		  "JIRA_HOST": "http://jira.tutk.com",
-		  "BITBUCKET_HTTP_HOST": "http://stash.tutk.com:7990",
-		  "BITBUCKET_SSH_HOST": "ssh://git@stash.tutk.com:7999"]
+		  "CONFLUENCE_HOST": "http://confluence.tutk.com/",
+		  "STASH_HTTP_HOST": git_http_host,
+		  "STASH_SSH_HOST": git_ssh_host,
+		  "BITBUCKET_HTTP_HOST": git_http_host,
+		  "BITBUCKET_SSH_HOST": git_ssh_host,
+		  "CRASHLYTICS_API_TOKEN": crashlytics_api_key,
+		  "CRASHLYTICS_BUILD_SECRET": crashlytics_build_secret]
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////
