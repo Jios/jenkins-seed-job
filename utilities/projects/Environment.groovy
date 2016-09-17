bd2035ee349e5faee55292791a185d822fecba53
jian <li822jh@gmail.com>
2016-09-17 18:42:40 +0800

    environment: add class

---
+++ utilities/projects/Environment.groovy
@@ -0,0 +1,22 @@
+package utilities.projects
+
+class Environment
+{
+	public String SRVM_CUSTOMER_IDS
+	public String SRVM_RELEASE_FOR
+	public String SRVM_RELEASE_BY
+	public String SRVM_PRODUCT_CATALOG
+	public String BUILD_PLATFORM
+	public String BUILD_OUTPUT_PATH
+	public String REPORT_PATH
+
+	Environment()
+	{
+
+	}
+
+	Environment(java.util.LinkedHashMap map)
+	{
+		
+	}
+}
