b7a35c3d30dfc6a9fb50a3565bddea62c2f10b40
jian <li822jh@gmail.com>
2016-09-20 00:16:40 +0800

    seed: code refactoring

--- seed.groovy
+++ seed.groovy
@@ -26,9 +26,7 @@ import lib.src.main.groovy.*
 //////////////////////////////
 
 
-/*
- *  credentials
- */
+// credentials
 def credentialID = 'abs_ssh'
 
 
@@ -81,115 +79,124 @@ def exportPropertiesFile(file_path)
     }
 }
 
+def getJobName(projectObject, repoObject)
+{
+    def jobName  = "$projectObject.name/${repoObject.name}"
 
-// loop through projects
+    return jobName
+}
+
+def setProject(projectObject)
+{
+    projectObject.setHost_http("${STASH_HTTP_HOST}")
+    projectObject.setHost_ssh("${STASH_SSH_HOST}")
+}
+
+def setRepo(repoObject)
+{
+    repoObject.setBranchNames("${BRANCH_NAMES}")
+    repoObject.setSchedule("${SCM_SCHEDULE}")
+    repoObject.setOutput_path('${BUILD_OUTPUT_PATH}')
+    repoObject.setReport_path('${REPORT_PATH}')
+}
+
+
+//////////////////////////////////////////////////////////////////////
+
+
 def ymlFiles = new File("${WORKSPACE}/yml-files/")
 
-ymlFiles.eachFileRecurse (FileType.FILES) { file ->
+ymlFiles.traverse(type: FileType.FILES, nameFilter: ~/.*yml$/) { file ->
 
-    // process yml file only
-    if(file.name.endsWith('.yml')) 
+    /**
+     *  Project: stash/bitbucket project
+     */
+
+    def file_path     = getFilePath(file)
+    def fileStream    = getFileStreamFromFilePath(file_path)
+    def projectObject = parseYamlFileWithStream(fileStream)
+
+    setProject(projectObject)
+
+    /**
+     *  folder
+     */ 
+
+    folder(projectObject.name)
     {
-        /*
-         *
-         *  Project: stash/bitbucket project
-         *
+        displayName(projectObject.name)
+        description("$projectObject.name project")
+    }
+
+    /**
+     *  list view
+     */
+
+    new CustomViews(
+        viewName: projectObject.name
+    ).createListView(this).with 
+    {
+        projectObject.repos.each { repoObject ->
+            jobs
+            {
+                def jobName = getJobName(projectObject, repoObject)
+                name(jobName)
+            }
+        }
+    }
+
+    projectObject.repos.each { repoObject ->
+      
+        setRepo(repoObject)
+
+        /**
+         *  Repository: jenkins job
          */
 
-        def file_path     = getFilePath(file)
-        def fileStream    = getFileStreamFromFilePath(file_path)
-        def projectObject = parseYamlFileWithStream(fileStream)
 
-        // folder
-        folder(projectObject.name)
+        def jobName  = getJobName(projectObject, repoObject)
+        def newJob   = job(jobName)
+
+        // base job
+        def defaults = new Defaults()
+
+        new Defaults(
+            projectObject: projectObject,
+            repoObject: repoObject
+        ).build(this).with 
         {
-            displayName(projectObject.name)
-            description("$projectObject.name project")
-        }
+            // wrappers
+            //Wrappers.setJiraRelease(delegate, jira_release_notes, jira_project_key, jira_release_version, jira_release_filter)
+            Wrappers.setSshAgent(delegate, credentialID)
 
-        // set project
-        projectObject.setHost_http("${STASH_HTTP_HOST}")
-        projectObject.setHost_ssh("${STASH_SSH_HOST}")
+            // triggers
+            Triggers.setTriggers(delegate, repoObject.schedule)
 
-        projectObject.repos.each { repoObject ->
-          
-            /*
-             *
-             *  Repository: jenkins job
-             *
-             */
+            // scm: git
+            SCM.setSCM(delegate, projectObject, repoObject, credentialID)
+            
+            // build steps
+            Steps steps = new Steps()
+            steps.setBuildScript(delegate, repoObject.build_command)
+            steps.setEnvInjectBuilder(delegate)
 
-            // set repo
-            repoObject.setBranchNames("${BRANCH_NAMES}")
-            repoObject.setSchedule("${SCM_SCHEDULE}")
-            repoObject.setOutput_path('${BUILD_OUTPUT_PATH}')
-            repoObject.setReport_path('${REPORT_PATH}')
-
-            // base job
-            def jobName  = "$projectObject.name/${repoObject.name}"
-            def newJob   = job(jobName)
-            def defaults = new Defaults()
-
-            new Defaults(
-                projectObject: projectObject,
-                repoObject: repoObject
-            ).build(this).with 
-            {
-                // wrappers
-                //Wrappers.setJiraRelease(delegate, jira_release_notes, jira_project_key, jira_release_version, jira_release_filter)
-                Wrappers.setSshAgent(delegate, credentialID)
-
-                // triggers
-                Triggers.setTriggers(delegate, repoObject.schedule)
-
-                // scm: git
-                SCM.setSCM(delegate, projectObject, repoObject, credentialID)
-                
-                // build steps
-                Steps steps = new Steps()
-                steps.setBuildScript(delegate, repoObject.build_command)
-                steps.setEnvInjectBuilder(delegate)
-
-                // publishers
-                Publishers publishers = new Publishers()
-                // -archive
-                publishers.setArchiveArtifacts(delegate, "$repoObject.output_path/*")
-                // -git
-                publishers.setGitPublisher(delegate, repoObject.name)
-                // -jira
-                publishers.setJiraIssue(delegate)
-                //publishers.setJiraVersion(delegate, jira_project_key)
-                // -srvm
-                publishers.setSRVMScript(delegate)
-                // -reports
-                //publishers.setPublishHtml(delegate, "Screenshots", "$repoObject.report_path/screenshots.html")
-                //publishers.setArchiveJunit(delegate, "$repoObject.report_path/report.xml")
-            }
-
+            // publishers
+            Publishers publishers = new Publishers()
+            // -archive
+            publishers.setArchiveArtifacts(delegate, "$repoObject.output_path/*")
+            // -git
+            publishers.setGitPublisher(delegate, repoObject.name)
+            // -jira
+            publishers.setJiraIssue(delegate)
+            //publishers.setJiraVersion(delegate, jira_project_key)
+            // -srvm
+            publishers.setSRVMScript(delegate)
+            // -reports
+            //publishers.setPublishHtml(delegate, "Screenshots", "$repoObject.report_path/screenshots.html")
+            //publishers.setArchiveJunit(delegate, "$repoObject.report_path/report.xml")
+            
             // schedule a job
             //queue(jobName)
-
-            // list view
-            listView(projectObject.name) 
-            {
-                jobs
-                {
-                    name(jobName)
-                }
-
-                recurse(true)
-                
-                columns 
-                {
-                    status()
-                    weather()
-                    name()
-                    lastSuccess()
-                    lastFailure()
-                    lastDuration()
-                    buildButton()
-                }
-            }
         }
     }
 }
