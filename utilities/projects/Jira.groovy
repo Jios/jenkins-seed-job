04e4c6318fa3c4164b17c9276b5194b14e3dd55b
jian <li822jh@gmail.com>
2016-09-23 12:07:39 +0800

    update Jira class properties

--- utilities/projects/Jira.groovy
+++ utilities/projects/Jira.groovy
@@ -2,8 +2,8 @@ package utilities.projects
 
 class Jira
 {
-	public String key
-	public String release_notes
+	public String key = 'ABS'
+	public String release_notes = ''
 	public String release_version = '1.0.0'
 	public String release_filter = '(Released, Closed)'
 }
