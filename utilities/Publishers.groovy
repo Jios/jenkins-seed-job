07366b12f5bcd2bcdfb2a8c507557a68fb5d537c
jian <li822jh@gmail.com>
2016-09-24 18:38:48 +0800

    publishers: remove directories and files first

--- utilities/Publishers.groovy
+++ utilities/Publishers.groovy
@@ -90,6 +90,10 @@ class Publishers
     void setSRVMScript(def context)
     {
     	def shell_script = '''cd ${WORKSPACE}
+							 |
+							 |echo "remove srvm and slack repos"
+							 |rm -rf srvm
+							 |rm -rf slack
     						 |
     						 |#git submodule add -b master http://stash.tutk.com:7990/scm/abs/srvm.git srvm
 							 |#cd srvm && git reset --hard && git pull && cd ..
@@ -98,10 +102,6 @@ class Publishers
 							 |git clone -b master http://stash.tutk.com:7990/scm/abs/slack.git slack
 							 |
 							 |python srvm/srvm.py
-							 |
-							 |echo "remove srvm and slack repos"
-							 |rm -rf srvm
-							 |rm -rf slack
 							 |'''.stripMargin()
 
     	context.publishers
