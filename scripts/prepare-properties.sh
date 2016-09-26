e2dbd514dc6d6e4d083d23afe60406e2660adfd7
jian li <jian_li@tutk.com>
2016-09-26 15:24:10 +0800

    add git_branch to postbuild

--- scripts/prepare-properties.sh
+++ scripts/prepare-properties.sh
@@ -9,3 +9,5 @@ touch $prebuild_path
 touch $postbuild_path
 
 python -c 'import os; git_branch = os.environ["GIT_BRANCH"].split("/"); repo = git_branch[0]; branch = "/".join(git_branch[1:]); f = open(os.environ["postbuild_path"], "a"); f.write("branch=" + branch + "\n"); f.write("repo=" + repo + "\n"); f.close()'
+
+echo GIT_BRANCH=${GIT_BRANCH} >> properties/postbuild.properties
