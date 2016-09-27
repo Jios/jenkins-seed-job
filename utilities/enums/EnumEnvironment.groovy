5731b5b0652342645335e91c7915068f11200de2
jian <li822jh@gmail.com>
2016-09-27 23:39:59 +0800

    add EnumEnvironment

---
+++ utilities/enums/EnumEnvironment.groovy
@@ -0,0 +1,12 @@
+package utilities.enums
+
+/**
+ * reference: https://github.com/cfpb/jenkins-automation/blob/master/src/main/groovy/jenkins/automation/utils/Environment.groovy
+ */
+
+enum EnumEnvironment
+{
+    development,
+    stage,
+    production
+}
