2ce9ae41db2568d9ed2c03ee0597f31317b34440
jian li <jian_li@tutk.com>
2016-10-03 18:18:16 +0800

    script: prebuild.properties sometimes cannot be generated

--- scripts/prepare-properties.py
+++ scripts/prepare-properties.py
@@ -12,8 +12,10 @@ if not os.path.exists(properties_path):
     os.makedirs(properties_path)
 
 postbuild_path = properties_path + "/postbuild.properties"
-touch(properties_path + "/prebuild.properties")
-touch(properties_path)
+prebuild_path  = properties_path + "/prebuild.properties"
+
+touch(prebuild_path)
+touch(postbuild_path)
 
 git_branch = os.environ["GIT_BRANCH"].split("/")
 repo = git_branch[0]
@@ -23,4 +25,7 @@ f = open(postbuild_path, "a")
 f.write("branch=" + branch + "\n")
 f.write("repo=" + repo + "\n")
 f.write("GIT_BRANCH=" + os.environ["GIT_BRANCH"] + "\n")
+f.close()
+
+f = open(prebuild_path, "a")
 f.close()
